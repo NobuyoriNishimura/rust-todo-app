52cabd16110d8d7df24b2e4eb3d4c03e529e5eb6
nobu <nobu0723ab@gmail.com>
2026-03-16 10:48:05 +0900

    feat: add mod 'hello.rs' and fix main.rs to use say_hello function.

--- todo-backend/src/main.rs
+++ todo-backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello Rustacean!");
+mod hello;
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/hello", get(crate::hello::say_hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
