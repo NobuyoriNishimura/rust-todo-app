9d7a01497e23eee7079e7c0a5c127ac4f8e1cf6b
nobu <nobu0723ab@gmail.com>
2026-03-21 20:54:52 +0900

    refactor: log when server starts

--- todo-backend/src/main.rs
+++ todo-backend/src/main.rs
@@ -32,6 +32,7 @@ async fn main() -> Result<(), sqlx::Error> {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+    println!("The server is ready.");
 
     Ok(())
 }
