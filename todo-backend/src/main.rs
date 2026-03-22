e406b42785403a21ec71ba43f98c2d1d127c5cd1
nobu <nobu0723ab@gmail.com>
2026-03-22 12:38:07 +0900

    fix: server readiness log

--- todo-backend/src/main.rs
+++ todo-backend/src/main.rs
@@ -31,8 +31,8 @@ async fn main() -> Result<(), sqlx::Error> {
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
     println!("The server is ready.");
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
