af052b574d544d29affb2f773102dc1767af742b
nobu <nobu0723ab@gmail.com>
2026-03-23 12:24:58 +0900

    feat: implement 'delete' function

--- todo-backend/src/todo.rs
+++ todo-backend/src/todo.rs
@@ -42,7 +42,16 @@ pub async fn add(State(state): State<AppState>, Json(payload): Json<NewToDo>) ->
 // delete TODO
 // inputs: ID
 // output: "Delete the TODO" or "Error: the TODO isn't deleted"
-pub async fn delete(State(state): State<AppState>, Path(id): Path<usize>) -> &'static str {}
+pub async fn delete(State(state): State<AppState>, Path(id): Path<usize>) -> &'static str {
+    let result = sqlx::query!("DELETE FROM todo_list WHERE id = ?", id)
+        .execute(&state.database_pool)
+        .await;
+
+    match result {
+        Ok(_) => "Delete the ToDo.",
+        Err(_) => "Error: the ToDo isn't deleted.",
+    }
+}
 
 // // check TODO
 // //inputs: None
